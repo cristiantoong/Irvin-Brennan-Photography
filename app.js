9b76195945a7e8562b05df9a7d4ad0163730aa68
unknown <cristiantoong@gmail.com>
2020-01-05 15:08:30 +0800

    added smooth scroll

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ links.forEach(link => {
     burger.classList.toggle('toggle');
   })
 })
+
+
+// Scroll To Top
+
+$(document).ready(function() {
+  // show hide button on scroll
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $('.scrollToTop').fadeIn();
+      } else {
+          $('.scrollToTop').fadeOut();
+      }
+  });
+
+  // smooth scrolling to top
+  $('.scrollToTop').click(function() {
+    $('html,body').animate({
+        scrollTop: 0
+    })
+})
+});
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 800);
+});
